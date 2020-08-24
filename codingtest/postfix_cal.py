f39672b79243cc2f2efc42ec837ebe66a66e43b3
dPfla0130 <wlddy1518@naver.com>
2020-08-24 17:52:20 +0900

    Initial commit

---
+++ codingtest/postfix_cal.py
@@ -0,0 +1,22 @@
+import sys
+#sys.stdin = open("in2.txt", "r")
+a = input()
+
+stack = []
+for i in a:
+    if i.isdecimal():
+        stack.append(int(i))
+    elif i.isdecimal()!=True:
+        a = stack.pop()
+        b = stack.pop()
+        if i=="+":
+            res = b + a
+        elif i=="-":
+            res = b - a
+        elif i=="*":
+            res = b * a
+        elif i=="/":
+            res = b / a
+        stack.append(res)
+for i in stack:
+    print(i)
